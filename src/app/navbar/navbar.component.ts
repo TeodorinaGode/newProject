efc0ad4002df228b39585b082af7d3ef01a8e38b
TeodorinaGode <teodorina.gode@gmail.com>
2022-06-23 20:33:15 +0200

    menu item

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -11,7 +11,58 @@ export class NavbarComponent implements OnInit {
     ) {
     }
 
+    public adminNavBarItems: Array<any> =[
+      {
+        label: 'Kategoritë',
+        subMenu: [
+          {
+            label: 'Pelena per femije',
+            subMenu: [
+              {
+                label: 'Michael Prentice',
+                link: '#',
+              },
+              {
+                label: 'Stephen Fluin',
+                link: '#',
+              },
+              {
+                label: 'Mike Brocchi',
+                link: '#',
+              },
+            ],
+          },
+          {
+            label: 'Produkte per nena shtatzen',
+            subMenu: [
+              {
+                label: 'Delight your Organization',
+                link: '#',
+              },
+              {
+                label: "What's up with the Web?",
+                link: '#',
+              },
+              {
+                label: 'My ally, the CLI',
+                link: '#',
+              },
+              {
+                label: 'Become an Angular Tailor',
+                link: '#',
+              },
+            ],
+          },
+          {
+            label: 'Produkte për nena pas lindjes',
+            link: '#',
+          },
+        ],
+      }
+    ];
+
     ngOnInit(): void {
+      console.log(this.adminNavBarItems);
     } 
 
 }
